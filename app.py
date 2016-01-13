5e6472e9bc4e9985cd3cab17fa77a6c9526f555b
cygairko <cygairko@gmail.com>
2016-01-13 22:28:26 +0100

    reformat

--- app.py
+++ app.py
@@ -1,38 +1,33 @@
 from datetime import date, datetime
-from math import radians as rad,degrees as deg
-import re
 
 from bottle import Bottle, route, run
 import ephem
-
-import json
-
-# import Moon
 
 
 @route('/hello')
 def hello():
     return "Hello World!"
 
+
 # <dateStr:re:(\d{4})[/.-](\d{1,2})[/.-](\d{1,2})>/
 # <latitude:re:(-?\d{1,2}[.]\d{6})>
 # <longitude:re:(-?\d{1,2}[.]\d{6})>
 
-@route('/moon/')
+@route('/moon')
 def moon():
-	m = ephem.Moon()
-	m.compute(datetime.utcnow())
-	
-	phase = m.moon_phase
-	
-	colong = m.colong
-	
-	nnm = ephem.next_new_moon(datetime.utcnow())
-	nfm = ephem.next_full_moon(datetime.utcnow())
-	
-	result = {'next_new_moon': str(nnm), 'next_full_moon': str(nfm), 'visibility': str(phase), 'colong': str(colong)}
-	
-	return result
+    m = ephem.Moon()
+    m.compute(datetime.utcnow())
+
+    phase = m.moon_phase
+
+    colong = m.colong
+
+    nnm = ephem.next_new_moon(datetime.utcnow())
+    nfm = ephem.next_full_moon(datetime.utcnow())
+
+    result = {'next_new_moon': str(nnm), 'next_full_moon': str(nfm), 'visibility': str(phase), 'colong': str(colong)}
+
+    return result
 
 
 run(host='localhost', port=61113, debug=True)
